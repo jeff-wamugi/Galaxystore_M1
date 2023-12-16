4597142054941da70518b1607e0409d7cb68080e
jeff-wamugi <78215278+jeff-wamugi@users.noreply.github.com>
2023-12-16 22:16:57 +0300

    Products module updated

--- ecommerce-admin/pages/api/products.js
+++ ecommerce-admin/pages/api/products.js
@@ -1,3 +1,27 @@
-export default function handle(req, res){
-    res.json(req);
+import { mongooseConnect } from "@/lib/mongoose";
+import { Product } from "@/models/Product";
+import mongoose from "mongoose";
+
+export default async function handle(req, res){
+    const {method} = req;
+    await mongooseConnect();
+
+    if (method === 'GET'){
+        if(req.query?.id){
+            res.json(await Product.findOne({_id:req.query.id}));
+        } else {
+            res.json(await Product.find());
+        }
+        
+    }
+
+
+    if (method === 'POST'){
+        const {title,description,price} = req.body;
+        const productDoc = await Product.create({
+            title,description,price,
+        })
+        res.json(productDoc);
+
+    }
 }
